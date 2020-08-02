8bd48da834757aca3fffaaf9b000de6134f3e326
AnzPhoenix <sunnypark554@gmail.com>
2020-08-02 06:43:48 -0700

    Added functionality

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>") # string of html code
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>") # string of html code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contacts</h1>")
